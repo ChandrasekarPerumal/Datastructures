25ee1a3813185e79225278d59d6925b607846844
ChandrasekarPerumal <chandruselv@gmail.com>
2023-09-07 18:03:33 +0530

    In-Order Traversal

--- DataStructures/src/main/java/com/dsa/dataStructures/tree/BinaryTree.java
+++ DataStructures/src/main/java/com/dsa/dataStructures/tree/BinaryTree.java
@@ -63,6 +63,24 @@ public class BinaryTree {
 		}
 	}
 
+	
+	
+	public static void inOrder(Node root) {
+		
+		if(root == null) {
+			return;			
+		}
+		
+		inOrder(root.left);
+		
+		System.out.println(root.data);
+		
+		inOrder(root.right);
+		
+		
+	}
+	
+	
 	public static void main(String ar[]) {
 		int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
@@ -73,8 +91,10 @@ public class BinaryTree {
 
 //		System.out.println(root.data);
 
-		preOrder(root);
+//		preOrder(root);
 
+		inOrder(root);
+		
 	}
 
 }
