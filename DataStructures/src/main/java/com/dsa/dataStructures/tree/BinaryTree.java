a08492634bc5a27bb4b60ab15be75c8068030c90
ChandrasekarPerumal <chandruselv@gmail.com>
2023-09-07 17:21:23 +0530

    Binary Tree Formation

---
+++ DataStructures/src/main/java/com/dsa/dataStructures/tree/BinaryTree.java
@@ -0,0 +1,61 @@
+package com.dsa.dataStructures.tree;
+
+public class BinaryTree {
+
+	static class Node {
+
+		// Declaration of Node elements
+		int data;
+		Node left;
+		Node right;
+
+		// Create a New-Node with left & right as null and return
+		public Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+
+	}
+
+	static class BinaryTr {
+		// -1: increment for each node insertion and take to length
+		// 0 = root node
+		static int indexForTraversal = -1;
+
+		public static Node buildBinaryTree(int nodes[]) {
+			indexForTraversal++;
+
+			if (nodes[indexForTraversal] == -1) {
+				return null;
+			}
+			
+			// First node will be built
+			Node newNode = new Node(nodes[indexForTraversal]);
+			
+			// Recursive call for left tree formation
+			newNode.left = buildBinaryTree(nodes);
+			
+			// Recursive call for right tree formation
+			newNode.right = buildBinaryTree(nodes);
+
+			return newNode;
+		}
+
+	}
+	
+	
+	public static void main(String ar[]) {
+	int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		
+		BinaryTr binaryTr = new BinaryTr();
+		
+		// Returns root node 
+		Node root = binaryTr.buildBinaryTree(nodes);
+		
+		System.out.println(root.data);
+		
+		
+	}
+
+}
